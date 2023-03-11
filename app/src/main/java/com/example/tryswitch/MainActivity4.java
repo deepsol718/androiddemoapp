c7745b98208d5538f5410f7eccdff27492df19ee
Deepak <deepaksolanki9759@gmail.com>
2023-03-11 11:38:29 +0530

    first commit

---
+++ app/src/main/java/com/example/tryswitch/MainActivity4.java
@@ -0,0 +1,20 @@
+package com.example.tryswitch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main5);
+    }
+
+    public void openMain(View view) {
+        startActivity(new Intent(this,MainActivity.class));
+    }
+}
